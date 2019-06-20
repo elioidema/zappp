58d881b599a6fa6e1d4de6089c175a5f4f95fcee
Wouter Kreischer <w.kreischer@gmail.com>
2019-06-20 02:03:46 +0200

    login screen

--- zappp-app/pages/index.vue
+++ zappp-app/pages/index.vue
@@ -1,26 +1,12 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        zappp-app
-      </h1>
-      <h2 class="subtitle">
-        My dazzling Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
+    <form class="bg-primary text-center w-1/3 px-3 py-4 text-white mx-auto rounded">
+      <input type="text" placeholder="Username" class="block w-full mx-auto text-sm py-2 px-3 rounded" />
+      <input type="text" placeholder="Password" class="block w-full mx-auto text-sm py-2 px-3 rounded my-3" />
+      <button class="bg-blue text-white font-bold py-2 px-4 rounded border block mx-auto w-full">
+        Login
+      </button>
+    </form>
   </div>
 </template>
 
--- zappp-app/tailwind.config.js
+++ zappp-app/tailwind.config.js
@@ -1,7 +1,10 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
   theme: {
-    extend: {}
+    colors: {
+      primary: '#197278',
+      secondary: '#6c6ea0',
+    }
   },
   variants: {}
 }
