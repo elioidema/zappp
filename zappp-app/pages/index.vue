aee63b31ca4209d5fa765aa13f619f1a29c0f117
Wouter Kreischer <w.kreischer@gmail.com>
2019-06-20 04:04:35 +0200

    axios

--- zappp-app/pages/index.vue
+++ zappp-app/pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container min-h-screen flex justify-center items-center text-center mx-auto">
-    <form class="bg-primary text-center w-1/3 px-3 py-4 text-white mx-auto rounded">
+    <form  v-on:submit.prevent="postLogin()" class="bg-primary text-center w-1/3 px-3 py-4 text-white mx-auto rounded">
       <input type="text" placeholder="Username" class="block w-full mx-auto text-sm py-2 px-3 rounded" />
       <input type="text" placeholder="Password" class="block w-full mx-auto text-sm py-2 px-3 rounded my-3" />
-      <button v-on:click.prevent="postLogin()" class="bg-blue text-white font-bold py-2 px-4 rounded border block mx-auto w-full">
+      <button class="bg-blue text-white font-bold py-2 px-4 rounded border block mx-auto w-full">
         Login
       </button>
     </form>
@@ -17,6 +17,13 @@ export default {
   methods: {
     postLogin() {
       console.log('test 11112233');
+        axios.post('http://192.168.8.118:8080/api/partyaccount/signIn', { login: 'w.kreischer@gmail.com', password: 'asdfasdf'})
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error.response, 'error ville')
+        })
     }
   }
 }
---
+++ zappp-app/pages/profile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container min-h-screen flex justify-center items-center text-center mx-auto">
+profile
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  methods: {
+
+  }
+}
+</script>
